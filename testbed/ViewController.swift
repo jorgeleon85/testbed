9beb777b086bf4cc1263d768699f1bb657909c22
Sianny Mata Sanchez <Siany@MacBook-Pro-de-Sianny.local>
2015-07-16 22:49:59 -0600

    Initial Commit

---
+++ testbed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testbed
+//
+//  Created by Sianny Mata Sanchez on 16/7/15.
+//  Copyright (c) 2015 Sianny Mata Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
