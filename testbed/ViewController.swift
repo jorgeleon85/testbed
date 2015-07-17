ede95172583c2e9fbc259caf071742c790c5ef34
Sianny Mata Sanchez <Siany@MacBook-Pro-de-Sianny.local>
2015-07-16 23:39:10 -0600

    stack message to main textview

--- testbed/ViewController.swift
+++ testbed/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let _version = "0.0.1"
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initTextView()
+        message("Testbed \(_version)")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    private func initTextView() {
+        textView.scrollEnabled = false
+        textView.scrollEnabled = true
+        
+        textView.font = UIFont(name: "Courier", size: 14.0)
+        textView.editable = false
+    }
+    
+    private func message(msg: String) {
+        textView.text = textView.text + msg + "\n"
+        textView.scrollRangeToVisible(NSRange(location: count(textView.text) - 1, length: 0))
+    }
+ 
 }
 
